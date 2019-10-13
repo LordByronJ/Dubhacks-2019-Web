d0433c17265e1d479b761e3f9566b2b090b9e02c
Vishal Devireddy <TwoTau@users.noreply.github.com>
2019-10-13 04:57:31 -0700

    Add firebase function returning palette list JSON

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,8 +1,27 @@
 const functions = require('firebase-functions');
+const {Firestore} = require('@google-cloud/firestore');
+const {Storage} = require('@google-cloud/storage');
+ 
+// Creates clients
+const firestore = new Firestore();
+const storage = new Storage();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloworld = functions.https.onRequest(async (req, res) => {
+	res.send('Hello!');
+});
+
+exports.getdocs = functions.https.onRequest(async (req, res) => {
+	const allImages = [];
+
+	let allImagesRef = await firestore.collection('images').get();
+	allImagesRef.forEach(img => {
+		let name = img._fieldsProto.name.stringValue;
+		name = name.substr(0, name.length-4).substr(0, 10) + name.substr(-4);
+		allImages.push({
+			'name': name,
+			'url': img._fieldsProto.url.stringValue,
+		});
+	});
+
+	res.send(allImages);
+});
